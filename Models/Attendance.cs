ef787d1950ffc4a75a70610c87befea1b4e3ba12
dilmuhammad <dizenstudios@gmail.com>
2025-10-14 16:15:33 +0500

    80801149

--- Models/Attendance.cs
+++ Models/Attendance.cs
@@ -8,9 +8,13 @@ namespace Bogcha.Models
         public int Id {get;set;}
         public int? ClientId {get;set;}
         public int? GroupId {get;set;}
-        public bool IsExist {get;set;}
-        public DateTime CreatedAt {get;set;} = DateTime.Now;
+        public bool IsExist {get;set;} = false;
+        public DateOnly CreatedAt {get;set;}
         public DateTime UpdatedAt {get;set;} = DateTime.Now;
+
+        public Attendance(){
+            CreatedAt = DateOnly.FromDateTime(DateTime.Now);
+        }
 
         public Client Client{get;set;}
         public Group Group{get;set;}
